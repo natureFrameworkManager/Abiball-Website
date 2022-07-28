1eda3dfaf21e3ce05e58c323ada3c6ff62cc7108
Caspar <mail@casparkroll.de>
2022-07-28 21:53:57 +0200

    Asset Img + File Orga + Intern collap

---
+++ js/collapse.js
@@ -0,0 +1,3 @@
+!!document.querySelector("#location-collapse-btn") ? document.querySelector("#location-collapse-btn").addEventListener("click", function() {
+    document.querySelector("#location-collapse-con").classList.toggle("hidden");
+}) : "";
